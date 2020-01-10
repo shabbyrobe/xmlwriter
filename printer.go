10fcfa5b4dfa1133a129e4696783fbd179f81da8
Blake Williams <code@shabbyrobe.org>
2020-01-11 02:43:51 +1100

    Presuming ASCII is the "common case", _much_ faster EscapeAttrString when attr values are ASCII only

--- printer.go
+++ printer.go
@@ -64,10 +64,35 @@ func (p *printer) cachedWriteError() error {
 	return err
 }
 
+var attrStringEscaped = [256]int{
+	'\'': 1,
+	'!':  1, '#': 1, '$': 1, '%': 1, '(': 1,
+	')': 1, '*': 1, '+': 1, ',': 1, '-': 1, '.': 1, '/': 1,
+	'0': 1, '1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1, '7': 1, '8': 1, '9': 1,
+	':': 1, ';': 1, '=': 1, '?': 1, '@': 1,
+	'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1, 'F': 1, 'G': 1, 'H': 1, 'I': 1, 'J': 1, 'K': 1, 'L': 1, 'M': 1, 'N': 1, 'O': 1, 'P': 1, 'Q': 1, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 1, 'W': 1, 'X': 1, 'Y': 1, 'Z': 1,
+	'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1, 'g': 1, 'h': 1, 'i': 1, 'j': 1, 'k': 1, 'l': 1, 'm': 1, 'n': 1, 'o': 1, 'p': 1, 'q': 1, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 1, 'w': 1, 'x': 1, 'y': 1, 'z': 1,
+	'[': 1, ']': 1, '^': 1, '_': 1, '`': 1, '~': 1,
+}
+
 func (p printer) EscapeAttrString(s string) error {
+	sz := len(s)
+	i := 0
+	for ; i < sz; i++ {
+		// Most of the strings k
+		if attrStringEscaped[s[i]] == 0 {
+			goto slow
+		}
+	}
+	p.WriteString(s)
+	return nil
+
+slow:
 	var esc []byte
-	last := 0
-	for i := 0; i < len(s); {
+	p.WriteString(s[:i])
+
+	last := i
+	for i < sz {
 		r, width := utf8.DecodeRuneInString(s[i:])
 		i += width
 		switch r {
--- printer_test.go
+++ printer_test.go
@@ -1,9 +1,40 @@
 package xmlwriter
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"strings"
 	"testing"
 	"unicode/utf8"
+
+	tt "github.com/shabbyrobe/xmlwriter/testtool"
 )
+
+func TestEscapeAttrString(t *testing.T) {
+	ws := func(t *testing.T, in string) (out string) {
+		t.Helper()
+		var b bytes.Buffer
+		p := printer{Writer: bufio.NewWriterSize(&b, 2048)}
+		p.EscapeAttrString(in)
+		p.Flush()
+		if err := p.cachedWriteError(); err != nil {
+			t.Fatal(err)
+		}
+		return b.String()
+	}
+
+	expect := func(t *testing.T, in string, out string) {
+		tt.Assert(t, ws(t, in) == out, fmt.Sprint(in, "!=", out))
+	}
+
+	expect(t, "abc", "abc")
+	expect(t, "a-c", "a-c")
+	expect(t, "a\nb", "a&#xA;b")
+	expect(t, "\nb", "&#xA;b")
+	expect(t, "a\n", "a&#xA;")
+}
 
 func TestIsInCharacterRange(t *testing.T) {
 	invalid := []rune{
@@ -16,5 +47,39 @@ func TestIsInCharacterRange(t *testing.T) {
 		if isInCharacterRange(r) {
 			t.Errorf("rune %U considered valid", r)
 		}
+	}
+}
+
+func BenchmarkEscapeAttrString(b *testing.B) {
+	for _, sz := range []int{10, 50, 300} {
+		b.Run(fmt.Sprintf("ascii-%d", sz), func(b *testing.B) {
+			p := printer{Writer: bufio.NewWriterSize(ioutil.Discard, 2048)}
+			v := strings.Repeat("1", sz)
+
+			for i := 0; i < b.N; i++ {
+				p.EscapeAttrString(v)
+				p.Reset(ioutil.Discard)
+			}
+		})
+
+		b.Run(fmt.Sprintf("utf8-first-%d", sz), func(b *testing.B) {
+			p := printer{Writer: bufio.NewWriterSize(ioutil.Discard, 2048)}
+			v := "\uD000" + strings.Repeat("1", sz-1)
+
+			for i := 0; i < b.N; i++ {
+				p.EscapeAttrString(v)
+				p.Reset(ioutil.Discard)
+			}
+		})
+
+		b.Run(fmt.Sprintf("utf8-last-%d", sz), func(b *testing.B) {
+			p := printer{Writer: bufio.NewWriterSize(ioutil.Discard, 2048)}
+			v := strings.Repeat("1", sz-1) + "\uD000"
+
+			for i := 0; i < b.N; i++ {
+				p.EscapeAttrString(v)
+				p.Reset(ioutil.Discard)
+			}
+		})
 	}
 }
